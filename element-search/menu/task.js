643030b10925b617d3afc1dba4705565e720a2c1
Logot1n <miwutka2@gmail.com>
2023-03-12 17:00:12 +0300

    Дз сделано 2 тема

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,24 @@
-const menuLinks = [...document.querySelectorAll(".menu__link")];
-const menuSub = [...document.querySelectorAll(".menu_sub")];
-const menuItems = [...document.querySelectorAll(".menu__item")];
+// Находим все саб меню
+const subMenus = [...document.querySelectorAll(".menu_sub")];
 
-menuLinks.forEach((link) => {
-    link.onclick = () => {
-        const menu = link.closest(".menu_sub");
-        if(menu) {
-            menu.classList.toggle("menu_active");
-        }
-        return false;
-    }
-})
+subMenus.forEach((subMenu) => {
+  // Находим родителя перебираемого меню и ищем у него все ссылки
+  const menuItem = subMenu.closest(".menu__item");
+  const subMenuLinks = menuItem.querySelectorAll(".menu__link");
+  // Перебираем ссылки и при нажатие на них, если ссылка находится внутри всплывающего меню - переход разрешен,
+  // если нет, то переход запрещен
+  subMenuLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      if (subMenu.contains(e.target)) {
+        return;
+      }
+      e.preventDefault();
+      // Проверка на открытие двух всплывающих окон
+      const activeMenu = document.querySelector(".menu_sub_active");
+      if (activeMenu && activeMenu !== subMenu) {
+        activeMenu.classList.remove("menu_sub_active");
+      }
+      subMenu.classList.toggle("menu_sub_active");
+    });
+  });
+});
