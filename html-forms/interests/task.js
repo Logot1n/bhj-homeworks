d545a7b5a150047d6bd5adf28cb5f210a79de0ed
Logot1n <miwutka2@gmail.com>
2023-03-21 20:46:38 +0300

    Исправление

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,17 +1,17 @@
 const interestUl = document.querySelector(".interests_main");
-interestUl.addEventListener('change', (event) => {
-    const interestItem = event.target.closest('.interest');
-    const interestItemCheck = interestItem.querySelector('.interest__check');
-    const interestItemUl = [...interestItem.querySelectorAll('.interests')];
+interestUl.addEventListener("change", (event) => {
+    const interestItem = event.target.closest(".interest");
+    const interestItemCheck = interestItem.querySelector(".interest__check");
+    const interestItemUl = [...interestItem.querySelectorAll(".interests")];
 
     interestItemUl.forEach(item => {
-        const childChecks = [...item.querySelectorAll('.interest__check')];
+        const childChecks = [...item.querySelectorAll(".interest__check")];
         childChecks.forEach(check => {
             if(interestItemCheck.checked) {
                 check.checked = true;
             } else {
                 check.checked = false;
             }
-        })
+        });
     });
 });
